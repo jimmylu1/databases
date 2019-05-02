931bd913443adc6c936de13bcc215c5f944bfffc
jimmylu1 <jimmy88lu@gmail.com>
2019-05-01 21:11:17 -0700

    Created tables, implement connection to server

--- server/models/index.js
+++ server/models/index.js
@@ -1,13 +1,30 @@
-var db = require('../db');
+var db = require('../db/index.js');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (){
+      // Pass in a callback
+      // Send query req
+        // Pass in query string, and callback(err,data)
+          // if err, console err
+          // Otherwise pass data into cb 
+    }, 
+    post: function () {
+      // Pass in req and callback
+        //send query req
+          //query string, callback(err, data)
+            //if err, console err
+            //otherwise pass data into cb
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
+    get: function () {},
+    post: function () {}
+  },
+
+  rooms: {
     get: function () {},
     post: function () {}
   }
--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,53 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+
+CREATE TABLE users (
+  id INT NOT NULL,
+  username VARCHAR(25),
+
+  PRIMARY KEY (id)
+); 
+
+CREATE TABLE rooms (
+  id INT NOT NULL,
+  roomname VARCHAR(25),
+  users_id INT NOT NULL,
+
+  FOREIGN KEY (users_id) 
+  REFERENCES users(id)
+  ON DELETE CASCADE,
+
+  PRIMARY KEY (id)
 );
 
+CREATE TABLE messages (
+  id INT NOT NULL,
+  msg VARCHAR(200),
+  users_id INT NOT NULL, 
+  rooms_id INT NOT NULL,
+  
+  FOREIGN KEY (users_id) 
+  REFERENCES users(id)
+  ON DELETE CASCADE,
+
+  FOREIGN KEY (rooms_id) 
+  REFERENCES rooms(id)
+  ON DELETE CASCADE,
+
+  PRIMARY KEY (id)
+); 
+
+
+
+
+
+
+  /* Describe your table here.*/
+
 /* Create other tables and define schemas for them here! */
-
-
-
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
